ceed1e4cbd0af76357d15e41eb6cbaa2ea7fbe00
pkumar1618 <prashanthch943@gmail.com>
2026-05-26 14:47:24 +0530

    install mysql server and backend server

--- 5-backend.sh
+++ 5-backend.sh
@@ -49,3 +49,12 @@ if [ $? -ne 0 ]; then
 else
     echo "Expense user already exists ... SKIPPED" | tee -a "$LOG_FILE"
 fi
+
+  mkdir -p /app
+  VALIDATE $? "Creating /app folder"
+
+  curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+  VALIDATE $? "Downloading backend application code"
+  cd /app
+  unzip /tmp/backend.zip
+  VALIDATE $? "Extracting"
